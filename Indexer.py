df3b58a17e46c4ef8aead909bc1efa1b495151fa
G4B5T3R <105610736+F4Z3M4N456@users.noreply.github.com>
2022-08-09 13:23:06 -0600

    Create Indexer.py

---
+++ Indexer.py
@@ -0,0 +1,9 @@
+print lista[0] # 1
+print lista[1] # 2.5
+print lista[2] # DevCode
+print lista[3] # [5,6]
+print lista[3][0] # 5
+print lista[3][1] # 6
+print lista[1:3] # [2.5, 'DevCode']
+print lista[1:6] # [2.5, 'DevCode', [5, 6], 4]
+print lista[1:6:2] # [2.5, [5, 6]]

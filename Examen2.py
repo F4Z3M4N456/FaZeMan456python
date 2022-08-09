bb8cfd5ba1c3c36adfbf218c3d0cb087bdce09d0
G4B5T3R <105610736+F4Z3M4N456@users.noreply.github.com>
2022-08-09 13:02:41 -0600

    Create Examen2.py

---
+++ Examen2.py
@@ -0,0 +1,12 @@
+#Este programa calcula el volumen :P
+
+print("Programa para calcular volumen")
+#esto solo es estetico
+n1=int(input("Ingrese la altura de su cilindro"))
+n2=int(input("Ingrese el radio"))
+#aqui asigno variables para pedirle al usuario y operar las variables luego en unsa sola operacion
+pi=3.14
+v=pi*n2**2*n1
+#he aqui la operacion
+print(v)
+#hacemos print para que pues no se vea xd

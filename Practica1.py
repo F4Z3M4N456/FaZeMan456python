4d542b7d9e66d2a60f2643ade408de5129007ed2
G4B5T3R <105610736+F4Z3M4N456@users.noreply.github.com>
2022-08-09 13:25:24 -0600

    Create Practica1.py

---
+++ Practica1.py
@@ -0,0 +1,24 @@
+n1=int(input("ingrese un numero"))
+n2=int(input("ingresa el segundo numero"))
+res=n1+n2
+print (res)
+
+n3=int(input("ingrese un numero"))
+n4=int(input("ingresa el segundo numero"))
+res=n1-n2
+print(res)
+
+n5=int(input("ingrese un numero"))
+n6=int(input("ingresa el segundo numero"))
+res=n1*n2
+print(res)
+
+n7=int(input("ingrese un numero"))
+n8=int(input("ingresa el segundo numero"))
+res8=n1%n2
+print(res)
+
+if res8 > 10:
+  print("es menor a diez")
+else:
+  print("es mayor a diez")

52b444977ddc563ad1cbf29b2e53248b82b96235
G4B5T3R <105610736+F4Z3M4N456@users.noreply.github.com>
2022-08-09 13:09:42 -0600

    Create Laboratorio5.py

---
+++ Laboratorio5.py
@@ -0,0 +1,8 @@
+vocales=["a","e", "i", "o","u"]
+str=(input("Ingrese una palabra"))
+#este programa era muy simple, tan solo hice una lista con vocales, y eso es todo lo que se necesitaba, si la palabra contenia una vocal, print que lo indicara, y si no, print que indique que no
+x=vocales[0]
+if x in vocales:
+  print("Comienza con una Vocal")
+else:
+  ("No comienza con una vocal")

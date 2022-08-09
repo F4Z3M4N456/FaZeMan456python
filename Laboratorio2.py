6e733ce3c35026d31fe4f9a1d33d9b35d7610478
G4B5T3R <105610736+F4Z3M4N456@users.noreply.github.com>
2022-08-09 13:10:43 -0600

    Create Laboratorio2.py

---
+++ Laboratorio2.py
@@ -0,0 +1,7 @@
+n1=str(input("Ingrese la palabra a evaluar"))
+if n1[-1] == n1[0]:
+  print("es palindromo")
+else:
+  print("No es palindromo")
+print(n1)
+#este tambien es simple, si la primera letra coincidia con la ultima de la palabra, hacia un print que lo indicara, esto se hizo con posiciones -1 y 0

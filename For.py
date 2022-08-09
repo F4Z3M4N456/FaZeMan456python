b1c577e366d52a19f14cf283e4c9596a40ae3af6
G4B5T3R <105610736+F4Z3M4N456@users.noreply.github.com>
2022-08-09 13:25:49 -0600

    Create For.py

---
+++ For.py
@@ -0,0 +1,8 @@
+n1=str(input("whos the imposter"))
+amongus=["red amongus","green amongus","yellow amongus","black amongus", "blue amongus","sussy baka"]
+for imposter in amongus:
+  print(imposter)
+if n1 =="yellow amongus":
+  print("saw him vent")
+else:
+ print("yellow amongas was not the impostor")ye

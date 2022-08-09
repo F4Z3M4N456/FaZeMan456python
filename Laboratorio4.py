d31d08be9d885c4d2799eccf669eeef24e1369fa
G4B5T3R <105610736+F4Z3M4N456@users.noreply.github.com>
2022-08-09 13:10:03 -0600

    Create Laboratorio4.py

---
+++ Laboratorio4.py
@@ -0,0 +1,5 @@
+N=int(input("Ingrese un numero"))
+R=int(input("Ingrese otro numero"))
+res=N**R
+print(res)
+#tan solo pedi 2 variables, el numero al que se le sacaria raiz, y la raiz, con ** se hace la potencia de variable uno y variable 2
